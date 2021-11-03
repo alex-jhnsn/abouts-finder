c98cc5eac8d439cb37e02379056b0dd6fc2bbb15
alex-jhnsn <alextheviking@hotmail.co.uk>
2021-11-03 12:46:39 +0000

    Add csv upload

--- options.js
+++ options.js
@@ -4,9 +4,13 @@ const $link = document.getElementById("txtLink");
 
 const $table = document.getElementById("tblData");
 
-const dataCache = [];
+let dataCache = [];
 
 chrome.storage.sync.get("data", (result) => {
+
+  if (!result.data)
+    return;
+
   dataCache.push(...result.data);
 
   dataCache.forEach((link, index) => {
@@ -14,14 +18,13 @@ chrome.storage.sync.get("data", (result) => {
   });
 });
 
-$addButton.addEventListener("click", () => {
+const add = () => {
   dataCache.push({ "Name": $name.value, "Link": $link.value });
   chrome.storage.sync.set({data: dataCache});
   addToTable(dataCache.length - 1, $name.value, $link.value);
-});
+}
 
 const remove = (e) => {
-  console.log("removing " + e.target.dataset.item);
   dataCache.splice(e.target.dataset.item, 1);
   chrome.storage.sync.set({data: dataCache});
   removeFromTable(e.target.dataset.item);
@@ -44,4 +47,27 @@ const addToTable = (index, name, link) => {
 const removeFromTable = (index) => {
   const target = document.getElementById("row-" + index);
   target.parentElement.removeChild(target);
-}
+};
+
+$addButton.addEventListener("click", add);
+
+fileData.addEventListener("change", (e) => {
+  console.log("Change");
+
+  Papa.parse(e.target.files[0], {
+    header: true,
+    skipEmptyLines: true,
+    complete: function(results) {
+      if (results.errors)
+        console.log("oops");
+
+      console.log(results);
+      
+      dataCache = results.data;
+      chrome.storage.sync.set({data: dataCache});
+      results.data.forEach((x, i) => {
+        addToTable(i, x.Name, x.Link);
+      });
+    }
+  });
+});
